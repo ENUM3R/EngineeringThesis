4736dbca759fab6ee0163ba9b795fffd7f48a6ea
CyprianKozubek <cypriankozubek@wp.pl>
2025-08-10 21:11:14 +0200

    frontend normal directory

---
+++ frontend_app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Starter!
+        </p>
+        <h3>Beatiful first react page</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
